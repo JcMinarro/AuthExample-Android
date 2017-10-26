61a50d93d17ca26f164eaf7da8e5aa5fc1b1b855
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-10-26 19:58:30 +0200

    Create log functions by buildtype

---
+++ app/src/debug/java/com/jcminarro/authexample/Utils.kt
@@ -0,0 +1,10 @@
+package com.jcminarro.authexample
+
+import android.util.Log
+
+val DEBUG_TAG = "debugTag"
+
+@JvmOverloads
+fun log(tag: String = DEBUG_TAG, text: String) {
+    Log.d(DEBUG_TAG, text)
+}
---
+++ app/src/release/java/com/jcminarro/authexample/Utils.kt
@@ -0,0 +1,8 @@
+package com.jcminarro.authexample
+
+import android.util.Log
+
+val DEBUG_TAG = "debugTag"
+
+@JvmOverloads
+fun log(tag: String = DEBUG_TAG, text: String) {}
