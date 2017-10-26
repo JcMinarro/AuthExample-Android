edecc05bb45deb3c6d3831c4d9513b28fefbe5d0
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-10-26 20:49:16 +0200

    Add network dependencies

--- buildSrc/src/main/groovy/com/jcminarro/Dependencies.groovy
+++ buildSrc/src/main/groovy/com/jcminarro/Dependencies.groovy
@@ -10,6 +10,9 @@ class Dependencies {
     private static String ESPRESSO_VERSION = '3.0.1'
     private static String DAGGER_VERSION = '2.8'
     private static String BUTTERKNIFE_VERSION = '8.5.1'
+    private static String RETROFIT_VERSION = '2.1.0'
+    private static String OK_HTTP_VERSION = '3.5.0'
+    private static String OK2CURL_VERSION = '0.3.2'
 
     static String kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
     static String androidBuildToolGradlePlugin = "com.android.tools.build:gradle:$ANDROID_BUILD_TOOL_VERSION"
@@ -22,4 +25,8 @@ class Dependencies {
     static String daggerCompiler = "com.google.dagger:dagger-compiler:$DAGGER_VERSION"
     static String butterKnife = "com.jakewharton:butterknife:$BUTTERKNIFE_VERSION"
     static String butterKnifeCompiler = "com.jakewharton:butterknife-compiler:$BUTTERKNIFE_VERSION"
+    static String retrofit = "com.squareup.retrofit2:retrofit:$RETROFIT_VERSION"
+    static String retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:$RETROFIT_VERSION"
+    static String okHttpLogger = "com.squareup.okhttp3:logging-interceptor:$OK_HTTP_VERSION"
+    static String ok2curl = "com.github.mrmike:Ok2Curl:$OK2CURL_VERSION"
 }
