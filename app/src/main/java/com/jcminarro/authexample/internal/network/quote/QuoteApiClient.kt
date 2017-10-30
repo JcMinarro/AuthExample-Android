e611d8812d3df49a438d5eabe60b0e43574e5675
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-10-30 17:36:42 +0100

    Notify that getRandomQuote method could throw an exception

--- app/src/main/java/com/jcminarro/authexample/internal/network/quote/QuoteApiClient.kt
+++ app/src/main/java/com/jcminarro/authexample/internal/network/quote/QuoteApiClient.kt
@@ -1,12 +1,14 @@
 package com.jcminarro.authexample.internal.network.quote
 
 import com.jcminarro.authexample.internal.model.Quote
+import com.jcminarro.authexample.internal.network.APIIOException
 import com.jcminarro.authexample.internal.network.ApiClient
 import javax.inject.Inject
 
 class QuoteApiClient @Inject
 constructor(endpoint: QuoteEndpoint) : ApiClient<QuoteEndpoint>(endpoint) {
 
+    @Throws(APIIOException::class)
     fun getRandomQuote(): Quote =
             map(evaluateCall(endpoint.randomQuote.execute()))
 }
