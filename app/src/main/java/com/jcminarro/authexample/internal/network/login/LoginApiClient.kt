0eecab47eebe12f57c7deb06d117fa415bc2d8ab
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-10-30 16:58:09 +0100

    Notify that login method could throw an exception

--- app/src/main/java/com/jcminarro/authexample/internal/network/login/LoginApiClient.kt
+++ app/src/main/java/com/jcminarro/authexample/internal/network/login/LoginApiClient.kt
@@ -1,5 +1,6 @@
 package com.jcminarro.authexample.internal.network.login
 
+import com.jcminarro.authexample.internal.network.APIIOException
 import com.jcminarro.authexample.internal.network.ApiClient
 import com.jcminarro.authexample.internal.network.OAuth
 import javax.inject.Inject
@@ -7,6 +8,7 @@ import javax.inject.Inject
 class LoginApiClient @Inject
 constructor(endpoint: LoginEndpoint) : ApiClient<LoginEndpoint>(endpoint) {
 
+    @Throws(APIIOException::class)
     fun login(username: String, password: String): OAuth =
             map(evaluateCall(endpoint.login(LoginBody(username, password)).execute()))
 }
