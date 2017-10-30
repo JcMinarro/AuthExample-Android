0cd02d330b050c0a9ddd64f6b610d4d3528290c1
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-10-30 17:50:04 +0100

    Add QuoteRepository tests

--- app/src/main/java/com/jcminarro/authexample/internal/repository/QuoteRepository.kt
+++ app/src/main/java/com/jcminarro/authexample/internal/repository/QuoteRepository.kt
@@ -3,7 +3,7 @@ package com.jcminarro.authexample.internal.repository
 import com.jcminarro.authexample.internal.network.APIIOException
 import com.jcminarro.authexample.internal.network.quote.QuoteApiClient
 
-class QuoteRespository(private val quoteApiClient: QuoteApiClient) {
+class QuoteRepository(private val quoteApiClient: QuoteApiClient) {
 
     @Throws(APIIOException::class)
     fun getRandomQuote() = quoteApiClient.getRandomQuote()
---
+++ app/src/test/java/com/jcminarro/authexample/internal/repository/QuoteRepositoryTest.kt
@@ -0,0 +1,36 @@
+package com.jcminarro.authexample.internal.repository
+
+import com.jcminarro.authexample.internal.model.Quote
+import com.jcminarro.authexample.internal.network.APIIOException
+import com.jcminarro.authexample.internal.network.quote.QuoteApiClient
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.doThrow
+import org.amshove.kluent.*
+import org.junit.Test
+
+class QuoteRepositoryTest {
+
+    private val quote = Quote("Linus Torvalds", "If you think your users are idiots only idiots will use it.")
+    private val quoteApiClient: QuoteApiClient = mock()
+    private val quoteRepository = QuoteRepository(quoteApiClient)
+
+    @Test
+    fun `Should return a random quote`() {
+        When calling quoteApiClient.getRandomQuote() doReturn quote
+
+        val result = quoteRepository.getRandomQuote()
+
+        result.author `should equal to` quote.author
+        result.message `should equal to` quote.message
+        Verify on quoteApiClient that quoteApiClient.getRandomQuote() was called
+    }
+
+    @Test(expected = APIIOException::class)
+    fun `Should throw an exception asking for a random quote`() {
+        When calling quoteApiClient.getRandomQuote() doThrow APIIOException(mock())
+        
+        quoteRepository.getRandomQuote()
+
+        Verify on quoteApiClient that quoteApiClient.getRandomQuote() was called
+    }
+}
