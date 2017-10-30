d8e80c8cd1771a166f05fb50db76a5e9c4c1736a
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-10-30 17:33:05 +0100

    Notify that refresh method could throw an exception

--- app/src/main/java/com/jcminarro/authexample/internal/network/refresh/RefreshApiClient.kt
+++ app/src/main/java/com/jcminarro/authexample/internal/network/refresh/RefreshApiClient.kt
@@ -1,5 +1,6 @@
 package com.jcminarro.authexample.internal.network.refresh
 
+import com.jcminarro.authexample.internal.network.APIIOException
 import com.jcminarro.authexample.internal.network.ApiClient
 import com.jcminarro.authexample.internal.network.OAuth
 import javax.inject.Inject
@@ -7,6 +8,7 @@ import javax.inject.Inject
 class RefreshApiClient @Inject
 constructor(endpoint: RefreshEndpoint) : ApiClient<RefreshEndpoint>(endpoint) {
 
+    @Throws(APIIOException::class)
     fun refresh(refreshToken: String): OAuth =
             map(evaluateCall(endpoint.refreshTokens(RefreshBody(refreshToken)).execute()))
 }
