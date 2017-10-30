92fdcb7b432c20ee3976bbf8e05f88d9dcf78792
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-10-30 16:59:43 +0100

    Create a repository to handle sessions

---
+++ app/src/main/java/com/jcminarro/authexample/internal/repository/SessionRepository.java
@@ -0,0 +1,41 @@
+package com.jcminarro.authexample.internal.repository;
+
+import com.jcminarro.authexample.internal.localdatasource.SessionDatasource;
+import com.jcminarro.authexample.internal.network.APIIOException;
+import com.jcminarro.authexample.internal.network.OAuth;
+import com.jcminarro.authexample.internal.network.login.LoginApiClient;
+import com.jcminarro.authexample.internal.network.refresh.RefreshApiClient;
+
+public class SessionRepository {
+
+    private final LoginApiClient loginApiClient;
+    private final RefreshApiClient refreshApiClient;
+    private final SessionDatasource sessionDatasource;
+
+    public SessionRepository(
+            LoginApiClient loginApiClient,
+            RefreshApiClient refreshApiClient,
+            SessionDatasource sessionDatasource) {
+        this.loginApiClient = loginApiClient;
+        this.refreshApiClient = refreshApiClient;
+        this.sessionDatasource = sessionDatasource;
+    }
+
+    public boolean login(String username, String password) throws APIIOException {
+        sessionDatasource.storeOAuthSession(loginApiClient.login(username, password));
+        return true;
+    }
+
+    public boolean refreshSession() {
+        OAuth oAuth = sessionDatasource.getOAuthSession();
+        if (oAuth != null) {
+            try {
+                sessionDatasource.storeOAuthSession(refreshApiClient.refresh(oAuth.getRefreshToken()));
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
